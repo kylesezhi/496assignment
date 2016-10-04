176dd9e19809067d2f590ec3dbd1a85f4e63028a
Kyle Bedell <kyle.bedell@gmail.com>
2016-10-04 16:41:46 -0400

    Templating works

--- base_page.py
+++ base_page.py
@@ -1,7 +1,16 @@
 import webapp2
+import os
+import jinja2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
+)
 
 
 class HelloWorld(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello World!')
+        template = JINJA_ENVIRONMENT.get_template('add.html')
+        self.response.write(template.render())
