bd95062cb8fe062f28f5a5913f79302f847b3557
Kyle Bedell <kyle.bedell@gmail.com>
2016-10-04 16:42:54 -0400

    Remove test and some commented code

--- main.py
+++ main.py
@@ -4,14 +4,3 @@ import webapp2
 app = webapp2.WSGIApplication([
     ('/', 'base_page.HelloWorld'),
 ], debug=True)
-
-# @app.route('/')
-# def hello():
-#     return 'Hello Worldt!'
-# 
-# 
-# @app.errorhandler(500)
-# def server_error(e):
-#     # Log the error and stacktrace.
-#     logging.exception('An error occurred during a request.')
-#     return 'An internal error occurred.', 500
